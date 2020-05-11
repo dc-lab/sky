f41a606bf5986771008fc4ffe76bb946448bc7e8
ilya-pauzner <ilya-pauzner@yandex.ru>
2020-05-12 00:27:51 +0300

    Added dummies.

--- job_manager/src/main.go
+++ job_manager/src/main.go
@@ -1,24 +1,20 @@
 package main
 
 import (
-	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		// an example API handler
-		err := json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
+	router.HandleFunc("/jobs", startJob).Methods(http.MethodPost)
+	router.HandleFunc("/jobs/{job_id}", getJob).Methods(http.MethodGet)
+	router.HandleFunc("/jobs", getJobs).Methods(http.MethodGet)
+	router.HandleFunc("/jobs/{job_id}/cancel", cancelJob).Methods(http.MethodPost)
+	router.HandleFunc("/jobs/{job_id}", deleteJob).Methods(http.MethodDelete)
 
 	srv := &http.Server{
 		Handler: router,
@@ -29,4 +25,36 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
+}
+
+func startJob(_ http.ResponseWriter, _ *http.Request) {
+	log.Printf("job started")
+}
+
+func getJobs(_ http.ResponseWriter, _ *http.Request) {
+	log.Printf("get jobs")
+}
+
+func getJob(w http.ResponseWriter, r *http.Request) {
+	jobId, ok := mux.Vars(r)["job_id"]
+	if !ok {
+		http.Error(w, "job_id not found in URL", http.StatusBadRequest)
+	}
+	log.Printf("get job with id = %s", jobId)
+}
+
+func cancelJob(w http.ResponseWriter, r *http.Request) {
+	jobId, ok := mux.Vars(r)["job_id"]
+	if !ok {
+		http.Error(w, "job_id not found in URL", http.StatusBadRequest)
+	}
+	log.Printf("cancel job with id = %s", jobId)
+}
+
+func deleteJob(w http.ResponseWriter, r *http.Request) {
+	jobId, ok := mux.Vars(r)["job_id"]
+	if !ok {
+		http.Error(w, "job_id not found in URL", http.StatusBadRequest)
+	}
+	log.Printf("delete job with id = %s", jobId)
 }
