9d0ef66e26dcc14484e20a73f5aeb41337065d76
Aleksei Basmanov <alex_kote@bk.ru>
2020-05-11 16:36:39 +0300

    Add logging

--- agent/src/network/send_data.go
+++ agent/src/network/send_data.go
@@ -10,6 +10,10 @@ import (
 	rm "github.com/dc-lab/sky/api/proto/resource_manager"
 )
 
+func printHardwareData(hwType string, hardwareData hardware.HardwareData) {
+	fmt.Printf("%s %vc %vb %vb\n", hwType, hardwareData.CpuCount, hardwareData.MemoryBytes, hardwareData.DiskBytes)
+}
+
 func SendRegistrationData(client rm.ResourceManager_SendClient, token string) {
 	request := rm.TGreetings{Token: token}
 	body := rm.TFromAgentMessage_Greetings{Greetings: &request}
@@ -19,10 +23,12 @@ func SendRegistrationData(client rm.ResourceManager_SendClient, token string) {
 
 func SendHardwareData(client rm.ResourceManager_SendClient, totalHardwareData hardware.HardwareData, freeHardwareData hardware.HardwareData) {
 	fmt.Println("Send hardware data")
+	printHardwareData("total: ", totalHardwareData)
+	printHardwareData("free: ", freeHardwareData)
 	totalHardware := pb.THardwareData{CoresCount: totalHardwareData.CpuCount, MemoryBytes: totalHardwareData.MemoryBytes, DiskBytes: totalHardwareData.DiskBytes}
 	freeHardware := pb.THardwareData{CoresCount: freeHardwareData.CpuCount, MemoryBytes: freeHardwareData.MemoryBytes, DiskBytes: freeHardwareData.DiskBytes}
 	response := rm.THardwareResponse{TotalHardwareData: &totalHardware, FreeHardwareData: &freeHardware}
-	body := rm.TFromAgentMessage_HardwareResponse{&response}
+	body := rm.TFromAgentMessage_HardwareResponse{HardwareResponse: &response}
 	err := client.Send(&rm.TFromAgentMessage{Body: &body})
 	common.DealWithError(err)
 }
@@ -45,7 +51,7 @@ func SendHealthChecks(client rm.ResourceManager_SendClient) {
 }
 
 func UpdateTasksStatuses(client rm.ResourceManager_SendClient) {
-	for ; ; time.Sleep(time.Microsecond * 100) {
+	for ; ; time.Sleep(time.Millisecond * 100) {
 		ConsumeTasksStatus(client, SendTaskData)
 	}
 }
