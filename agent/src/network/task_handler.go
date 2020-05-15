1b02288642dc1f970c9e24c9c4cfa6e0c32c83c4
Loud_Scream <Gleb-Tretyakov@users.noreply.github.com>
2020-05-15 10:49:16 +0300

    Agent task execution in docker (#42)
    * Add local and docker executors + main interface
    
    * Pass docker working dir
    
    * Cancel and delete docker execution

--- agent/src/network/task_handler.go
+++ agent/src/network/task_handler.go
@@ -2,10 +2,8 @@ package network
 
 import (
 	"fmt"
-	"github.com/dc-lab/sky/agent/src/common"
 	pb "github.com/dc-lab/sky/api/proto/common"
 	rm "github.com/dc-lab/sky/api/proto/resource_manager"
-	"os/exec"
 )
 
 func ConsumeTasksStatus(client rm.ResourceManager_SendClient, consumer func(rm.ResourceManager_SendClient, string, *pb.TResult)) {
@@ -37,44 +35,5 @@ func DeleteTask(taskId string) {
 	task, flag := GlobalTasksStatuses.Load(taskId)
 	if flag {
 		task.Delete()
-	}
-}
-
-func RunShellCommand(
-	command string,
-	directory string,
-	stdOutFilePath string,
-	stdErrFilePath string,
-	beforeExecution func(pid int64, result *pb.TResult),
-	afterExecution func(err error),
-	quit <-chan struct{},
-) {
-	cmd := exec.Command("/bin/sh", "-c", command)
-	cmd.Dir = directory
-	stdoutFile := common.CreateFile(stdOutFilePath)
-	defer stdoutFile.Close()
-	cmd.Stdout = stdoutFile
-	stderrFile := common.CreateFile(stdErrFilePath)
-	defer stderrFile.Close()
-	cmd.Stderr = stderrFile
-	err := cmd.Start()
-	common.DealWithError(err)
-	pid := int64(cmd.Process.Pid)
-	result := pb.TResult{ResultCode: pb.TResult_RUN}
-	if beforeExecution != nil {
-		beforeExecution(pid, &result)
-	}
-	done := make(chan error, 1)
-	go func() {
-		done <- cmd.Wait()
-	}()
-	select {
-	case <-quit:
-		err := cmd.Process.Kill()
-		common.DealWithError(err)
-	case err := <-done:
-		if afterExecution != nil {
-			afterExecution(err)
-		}
 	}
 }
