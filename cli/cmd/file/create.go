1491f96b9c1530a442326c1ae6f95c0019f5ba85
BigRedEye <mail@bigredeye.me>
2020-06-06 13:15:27 +0300

    Data manager client (#78)
    * feat(dm): API wrapper client library
    
    * feat(dm): WIP CLI commands for DM
    
    * flush

---
+++ cli/cmd/file/create.go
@@ -0,0 +1,62 @@
+package file
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/dc-lab/sky/cli/utils"
+	dm_client "github.com/dc-lab/sky/data_manager/client"
+)
+
+var CreateCmd = &cobra.Command{
+	Use:   "create --name <NAME> --file <PATH>",
+	Short: "create file",
+	Long: `Create file
+Returns created file id`,
+	Args: cobra.ExactArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+		url := utils.GetSkyUrl(cmd)
+		token := utils.GetUserToken(cmd)
+		name := utils.GetVariable(cmd, "name")
+		file := utils.GetVariable(cmd, "file")
+
+		var reader *os.File
+		if file == "-" {
+			reader = os.Stdin
+		} else {
+			var err error
+			reader, err = os.Open(file)
+
+			if err != nil {
+				log.Fatalf("Failed to open file: %s", err.Error())
+			}
+		}
+		defer reader.Close()
+
+		client, err := dm_client.MakeClient(url, token)
+		if err != nil {
+			log.Fatalf("Failed to connect to the platform: %s", err.Error())
+		}
+
+		res, err := client.CreateFileWithContents(&dm_client.File{
+			Name: name,
+		}, reader)
+		if err != nil {
+			log.Fatalf("Failed to upload file: %s", err.Error())
+		}
+
+		fmt.Printf("Successfully uploaded file")
+		fmt.Printf("ID: %s", res.Id)
+	},
+}
+
+func init() {
+	CreateCmd.Flags().StringP("name", "n", "", "Name of file")
+	CreateCmd.MarkFlagRequired("name")
+
+	CreateCmd.Flags().StringP("file", "f", "", "File to upload")
+	CreateCmd.MarkFlagRequired("file")
+}
---
+++ data_manager/client/go.mod
@@ -0,0 +1,9 @@
+module github.com/dc-lab/sky/data_manager/master
+
+go 1.14
+
+require (
+	github.com/dc-lab/sky/api/proto/common v0.0.0-20200605035819-85d768e90bb8 // indirect
+	github.com/dc-lab/sky/api/proto/data_manager v0.0.0-20200605035819-85d768e90bb8
+	google.golang.org/grpc v1.29.0
+)
